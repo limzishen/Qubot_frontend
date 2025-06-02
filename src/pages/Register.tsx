64d85f5d51940c29693e810e4594cb9a62d50df0
limzishen <limzishen27@gmail.com>
2025-06-02 12:26:52 +0800

    backend intergration

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,45 @@
 // src/pages/Register.tsx
 import React from "react";
 import { Box, Typography, TextField, Button, Paper } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
+import axios from 'axios';
 
 const Register: React.FC = () => {
+  const navigate = useNavigate();
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    //console.log("Sending data:", { userName: name, email, password });
+
+    try {
+      const response = await axios.post('http://localhost:4000/api/users/signup', {
+          userName: name,
+          email,
+          password
+        }, {
+          withCredentials: true
+        }
+      );
+        setTimeout(() => {
+            navigate("/login");
+        }, 2000);
+    } catch (error) {
+      console.error(error);
+      alert("Registration failed. Please try again.");
+    }
+  };
+  
+
+
   return (
     <Box 
       sx={{ 
@@ -36,8 +72,21 @@ const Register: React.FC = () => {
           </Typography>
         </Box>
 
-        <Box component="form" noValidate sx={{ mt: 2 }}>
+        <Box component="form" noValidate sx={{ mt: 2 }} onSubmit={handleSubmit}>
           {/* Add registration form fields here */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="User Name"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            sx={{ "& .MuiOutlinedInput-root": { borderRadius: "12px" } }}
+            onChange={(e) => setName(e.target.value)}
+            value={name} 
+          />
           <TextField
             margin="normal"
             required
@@ -46,8 +95,9 @@ const Register: React.FC = () => {
             label="Email Address"
             name="email"
             autoComplete="email"
-            autoFocus
             sx={{ "& .MuiOutlinedInput-root": { borderRadius: "12px" } }}
+            onChange={(e) => setEmail(e.target.value)}
+            value={email} 
           />
           <TextField
             margin="normal"
@@ -58,6 +108,8 @@ const Register: React.FC = () => {
             type="password"
             id="password"
             autoComplete="new-password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password} 
             sx={{ "& .MuiOutlinedInput-root": { borderRadius: "12px" } }}
           />
           <TextField
@@ -69,6 +121,8 @@ const Register: React.FC = () => {
             type="password"
             id="confirmPassword"
             autoComplete="new-password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
             sx={{ "& .MuiOutlinedInput-root": { borderRadius: "12px" } }}
           />
           
